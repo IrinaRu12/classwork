84d4b5ef456a0c1a12413c4a051aa7be2ddd657a
IrinaRu12 <163992434+IrinaRu12@users.noreply.github.com>
2024-03-26 12:02:03 +0300

    практическая 8 от 26.03.24

---
+++ Практическая 8 Румянцева.py
@@ -0,0 +1,39 @@
+coding utf-8
+задача №1
+a = int(input "Введите первое число" ))
+b = int(input "Введите второе число" ))
+c = int(input "Введите второе число" ))
+if a/2
+    print ("YES")
+else:
+    print ("NO")
+if b/2
+    print ("YES")
+else:
+    print ("NO")
+if c/2
+    print ("YES")
+else:
+    print ("NO")
+
+coding utf-8
+задача №2
+sing(x) = int(input "Введите число" ))
+if х>0
+    print ("значение x положительное")
+if х<0
+    print ("значение x отрицательное")
+    if х=0
+    print ("значение x=0")
+    
+coding utf-8
+задача №3
+d = int(input "Введите первое число в порядке возрастения" ))
+e = int(input "Введите второе число в порядке возрастения" ))
+f = int(input "Введите второе число в порядке возрастания" ))
+d<= e <= f
+if d<=e
+if e <= f
+if d<= f
+else
+    print ("Числа стоят в порядке возрастания")
