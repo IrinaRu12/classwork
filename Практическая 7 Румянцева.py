cafe577e1be0f9f4b1ac2673a79b2763473347df
IrinaRu12 <163992434+IrinaRu12@users.noreply.github.com>
2024-03-26 11:34:45 +0300

    практическая 7 от 22.03.2024

---
+++ Практическая 7 Румянцева.py
@@ -0,0 +1,28 @@
+coding utf-8
+задача №1
+a = int(input "Введите первое число" ))
+b = int(input "Введите второе число" ))
+if a>b 
+    print  ("Первое число больше")
+else:
+    print  ("Второе число больше")
+
+coding utf-8
+задача №2
+c = int(input "Введите первое число" ))
+d = int(input "Введите второе число" ))
+e = int(input "Введите второе число" ))
+p = (c+d+e)/2
+S = sqrt(p*(p-c)*(p-d)*(p-e))
+if S существует
+    print ("YES")
+else:
+    Print ("NO")         
+
+coding utf-8
+задача №3
+g = int(input "Введите натуральное чиссло" ))
+if g/4
+    print ("YES")
+else:
+    print ("NO")    
